d13b5100b017b4ff3c34d86daaa3f5bb0cbc6472
Soleny Yumare <solenyyumare@gmail.com>
2022-07-12 10:58:18 +0100

    new features 2.0

--- src/components/HomeComponent.vue/Pics.vue
+++ src/components/HomeComponent.vue/Pics.vue
@@ -6,56 +6,32 @@
     libanesa.
   </p>
   <div class="img">
-    <img
-      data-aos="fade-left"
-      data-aos-duration="1700"
-      class="hummus"
-      src="../../assets/hummus.png"
-      alt=""
-    />
+    <img class="hummus" src="../../assets/hummus.png" alt="" />
     <p class="position">
       El Líbano es uno de los clásicos de la comida árabe en Santa Cruz de
       Tenerife.
     </p>
   </div>
   <div class="img-2">
-    <img
-      data-aos="fade-right"
-      data-aos-duration="1700"
-      class="tabule"
-      src="../../assets/taboule.png"
-      alt=""
-    />
+    <img class="tabule" src="../../assets/taboule.png" alt="" />
   </div>
   <p class="position-kipe">
     La más exquisita, sana y exclusiva cocina mediterránea.
   </p>
   <div class="img">
-    <img
-      class="falafel"
-      src="../../assets/falafel.png"
-      alt=""
-      data-aos="fade-left"
-      data-aos-duration="1700"
-    />
+    <img class="falafel" src="../../assets/falafel.png" alt="" />
   </div>
   <div class="img-2">
     <p class="position-tabule">
       El Líbano tiene 22 mesas, capaces de reunir a casi un centenar de
       comensales.
     </p>
-    <img
-      data-aos="fade-right"
-      data-aos-duration="1700"
-      class="kipenaye"
-      src="../../assets/kipenaye.png"
-      alt=""
-    />
+    <img class="kipenaye" src="../../assets/kipenaye.png" alt="" />
     <p class="position-kipe">
       Avalados por los más de 42 años que llevamos abiertos.
     </p>
   </div>
-  <div class="img">
+  <div class="last-margin-jaruf">
     <img class="jaruf" src="../../assets/jaruf.png" alt="" />
   </div>
   <!--   <p class="text-2">
@@ -66,7 +42,8 @@
 <script></script>
 <style scoped>
 .title {
-  margin-top: 50px;
+  margin-top: 40px;
+  margin-bottom: 20px;
   text-align: center;
   font-size: 40px;
   letter-spacing: -4px;
@@ -91,7 +68,7 @@
   font-size: 19.5px;
   color: #3f331e;
   text-align: right;
-  padding: 10px;
+  padding: 20px;
 }
 .position-kipe {
   font-family: "Roboto Condensed", sans-serif;
@@ -105,7 +82,7 @@
   font-size: 19.5px;
   color: #3f331e;
   text-align: left;
-  padding: 0px 90px 10px 20px;
+  padding: 0.5px 90px 10px 20px;
 }
 .hummus {
   width: 450px;
@@ -123,6 +100,7 @@
   width: 450px;
   position: relative;
   margin-top: 400px;
+  right: 1%;
   filter: drop-shadow(0 0 0.25rem grey);
 }
 .img-2 {
@@ -141,5 +119,8 @@
   right: 33%;
   margin-top: -90px;
   filter: drop-shadow(0 0 0.25rem grey);
+}
+.last-margin-jaruf {
+  margin-bottom: -70px;
 }
 </style>
